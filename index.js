d0289c196f52b6dba2b7c3a2bbae9b842778303e
Ridwanullah <aladebaruwa@gmail.com>
2023-05-20 18:46:51 -0700

    add js

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log("Hello World")
+
+let name="John";
+
+let admin= name;
+alert(admin)
+
+const myInt = 5;
+const myFloat = 6.667;
+myInt;
+myFloat;
+typeof myFloat;
+typeof myInt;
+
+const lotsOfDecimal = 1.766584958675746364;
+lotsOfDecimal;
+const twoDecimalPlaces = lotsOfDecimal.toFixed(2);
+twoDecimalPlaces;
+
+
+let myNumber = "74";
+myNumber = Number(myNumber) + 3;
+alert(myNumber)
